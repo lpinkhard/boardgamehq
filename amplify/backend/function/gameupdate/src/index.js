ebace52a0015289c9378f1b44a14fcc3b2244a8b
Lionel Pinkhard <lionelpinkhard@me.com>
2023-07-08 18:02:43 +0200

    Change Sentry ID

--- amplify/backend/function/gameupdate/src/index.js
+++ amplify/backend/function/gameupdate/src/index.js
@@ -34,7 +34,7 @@ const documentClient = new aws.DynamoDB.DocumentClient();
 let currentPage = 0;
 
 Sentry.AWSLambda.init({
-    dsn: "https://e818cb73b2014d50af1f6517b9417de4@o4505493834563584.ingest.sentry.io/4505493836333056",
+    dsn: "https://509797279a334837b2123f7c0b76dc98@o4505493834563584.ingest.sentry.io/4505494618505216",
     tracesSampleRate: 1.0,
 });
 
